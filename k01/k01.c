3524b7d7f4bc0441b5108a74cfd462ba27605a3f
kakeru-eshita <bb35319010@ms.nagasaki-u.ac.jp>
2020-10-15 11:09:43 +0900

    k01提出1回目

--- k01/k01.c
+++ k01/k01.c
@@ -65,17 +65,36 @@ int LoadData(City arrayCity[])
 int LinearSearch(int key, City arrayCity[], int size)
 {
     //  ここを実装する
-
-
+ int pos, result;
+ for(pos=0; pos < size; pos++){
+    if(key == arrayCity[pos].id){
+       result=pos;
+    break;
+    }
+ }
+ return result;
 }
 
 int BinarySearch(int key, City arrayCity[], int left, int right)
 {
     //  ここを実装する
-
-
+ int pos, result;
+ result=-1;
+ left = 0;
+ right = MAX_CITY-1;
+    while (left <= right){
+      pos = left + (right - left) / 2;
+        if(key == arrayCity[pos].id){
+        result = pos;
+        break;
+        }else if(key >= arrayCity[pos].id){
+        left = pos + 1;
+        }else{
+        right = pos - 1;
+        }
+   }
+   return result;
 }
-
 
 
 int main(void)
